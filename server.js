6b3baa419a8620e2b25511ac03a2412601de571e
Kent Ketter <kent.ketter1@Gmail.com>
2018-11-30 15:59:00 -0800

    updates

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+//express documentation requires the previous two lines
+
+app.listen(3000), function() {
+  console.log('listening on port 3000');
+  
+});
